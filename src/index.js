22f9ea88f19555377566526d880924b81160332a
Marco Lepore <lepore86@gmail.com>
2017-12-16 12:02:10 +0100

    Add support for assets management with webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// css import test
+import './style.css';
+
+// image import test
+import icon from './icon.png';
+console.log(icon);
+
 // bundle test
 const hello = () => 'Hello world';
 console.log(hello());
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
         test: /\.js$/,
         use: ['babel-loader'],
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
       }
     ]
   },
