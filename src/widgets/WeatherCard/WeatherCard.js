27ea6e87c970635194285090d1df50171faf54ec
Marco Lepore <lepore86@gmail.com>
2017-12-18 00:01:46 +0100

    Build connected weather card

--- src/widgets/WeatherCard/WeatherCard.js
+++ src/widgets/WeatherCard/WeatherCard.js
@@ -2,31 +2,49 @@ import React from 'react';
 import Front from './Front';
 import Back from './Back';
 import flippable from './hoc/flippable';
+import withGeolocation from './hoc/withGeolocation';
+import withWeatherData from './hoc/withWeatherData';
+import { compose } from './utils/generic';
+
 import './components/WeatherIcon';
-
-const placeholderFrontProps = {
-  weather: {
-    temperature: 34,
-    wind: 10,
-    perceived: 36,
-    humidity: 80,
-    iconCode: 'yahoo-14',
-    description: 'Sunny'
-  },
-  place: 'Italy',
-  dayOfWeek: 2
-};
-
-const placeholderBackProps = {
-  latitude: 41.9,
-  longitude: 12.49
-};
 
 const WeatherCard = flippable(props => ({
   width: 255,
   height: 365
 }))(Front, Back);
 
-export default props => (
-  <WeatherCard {...placeholderFrontProps} {...placeholderBackProps} />
+const fahrenheitToCelsius = f => (f - 32) / 1.8;
+
+const extractWeatherData = ({
+  item: { condition: { temp, code, text } },
+  atmosphere: { humidity },
+  wind: { speed }
+}) => ({
+  temperature: fahrenheitToCelsius(parseFloat(temp)),
+  perceived: fahrenheitToCelsius(parseFloat(temp)),
+  wind: parseFloat(speed),
+  humidity: parseFloat(humidity),
+  iconCode: 'yahoo-' + code,
+  description: text
+});
+
+const enhance = compose(
+  withGeolocation(position => ({
+    latitude: position.latitude,
+    longitude: position.longitude
+  })),
+  withWeatherData(
+    data => ({
+      weather: extractWeatherData(data),
+      place: data.location.city
+    }),
+    props => ({
+      latitude: props.latitude,
+      longitude: props.longitude
+    })
+  )
 );
+
+const ConnectedWeatherCard = enhance(WeatherCard);
+
+export default props => <ConnectedWeatherCard />;
