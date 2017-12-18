12681677dc40f29e94c42146354329a0c4cd2385
Marco <lepore86@gmail.com>
2017-12-18 10:07:38 +0100

    Add autoprefixer to the build pipeline

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,7 @@ const config = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
